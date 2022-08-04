7b43f080981a24727611eee44a8e7f6bd7d0dd45
Leane Verhulst <lverhulst@gmail.com>
2022-08-04 16:33:06 -0500

    Add person social media links

--- src/components/Person.js
+++ src/components/Person.js
@@ -30,7 +30,15 @@ const Person = () => {
     }
     return false;
   });
-  const personlinks = person.links.facebook ? <a href={person.links.facebook}>{person.links.facebook}</a> : "";
+  const personlinkstwitter = person.links.twitter ? <a href={person.links.twitter}>{person.links.twitter}</a> : "";
+  const personlinksfacebook = person.links.facebook ? <a href={person.links.facebook}>{person.links.facebook}</a> : "";
+  const personlinkswebsite = person.links.website ? <a href={person.links.website}>{person.links.website}</a> : "";
+  const personlinksinstagram = person.links.instagram ? <a href={person.links.instagram}>{person.links.instagram}</a> : "";
+  const personlinkstwitch = person.links.twitch ? <a href={person.links.twitch}>{person.links.twitch}</a> : "";
+  const personlinksyoutube = person.links.youtube ? <a href={person.links.youtube}>{person.links.youtube}</a> : "";
+  const personlinkstiktok = person.links.tiktok ? <a href={person.links.tiktok}>{person.links.tiktok}</a> : "";
+  const personlinkslinkedin = person.links.linkedin ? <a href={person.links.linkedin}>{person.links.linkedin}</a> : "";
+  const personlinksother = person.links.othersocialmedia ? <a href={person.links.othersocialmedia}>{person.links.othersocialmedia}</a> : "";
   return (
     <div className="person">
       <h2 className="person-name">
@@ -42,7 +50,15 @@ const Person = () => {
         className="person-bio"
         dangerouslySetInnerHTML={{ __html: safeBio }}
       />
-      <div className="person-links">{personlinks}</div>
+      <div className="person-links">{personlinkstwitter}</div>
+      <div className="person-links">{personlinksfacebook}</div>
+      <div className="person-links">{personlinkswebsite}</div>
+      <div className="person-links">{personlinksinstagram}</div>
+      <div className="person-links">{personlinkstwitch}</div>
+      <div className="person-links">{personlinksyoutube}</div>
+      <div className="person-links">{personlinkstiktok}</div>
+      <div className="person-links">{personlinkslinkedin}</div>
+      <div className="person-links">{personlinksother}</div>
       <ProgramList program={filteredProgram} />
     </div>
   );
