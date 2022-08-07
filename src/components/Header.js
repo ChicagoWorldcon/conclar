880725953736cb933a41cc6ffffb6602f9643b8c
Leane Verhulst <lverhulst@gmail.com>
2022-08-07 16:26:08 -0500

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import PropTypes from "prop-types";
 import configData from "../config.json";
 
+const headerImg = configData.HEADER.IMG_SRC ? <img src={configData.HEADER.IMG_SRC} alt={configData.HEADER.IMG_ALT_TEXT}></img> : "";
 const showBreak = configData.HEADER.LINEFEED_AFTER_URL ? <br /> : "";
 
 const Header = ({ title }) => {
   document.title = title;
   return (
     <header>
-      <img src={configData.HEADER.IMG_SRC} alt={configData.HEADER.IMG_ALT_TEXT}></img>
+      {headerImg}
       {showBreak}
       <h1>{title}</h1>
     </header>
